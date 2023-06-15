4efc9fdff1465e84ffceca8344d18c10866cf0f3
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-15 06:37:37 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line"
+*Use the function puts
+* not allowed to use printf
+* program should end with the value 0
+*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle, \n");
 	return (0);
 }
 
