9d9ab75a9e4e737283e9e9b48a5aaf48145dc6b2
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-15 06:14:08 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle,\n");
-	return (0);}
+	printf("Programming is like building a multilingual puzzle");
+	return (0);
+}
 
