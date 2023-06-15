4314bc16881ed0025c887e8e222080b4f1c98b8a
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-15 07:05:17 -0700

    second code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Use the function printf
+*
+* Description: using the main function 
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
