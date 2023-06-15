b81549edce3fa0e37834645be2dbac57e092bf20
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-15 06:45:21 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*Write a C program that prints exactly "Programming is like building a multilingual puzzle"
+*Write a C programm 
 *Use the function puts
 * not allowed to use printf
 * program should end with the value 0
