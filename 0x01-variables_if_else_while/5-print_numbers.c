58ebbdf6b05789dcfe0228453902d1a8c32fae0f
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-16 07:41:38 -0700

    print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * main - prints all single digit numbers of base
- * Return: 0 (Success)
+ * Return:0 (Success)
  */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
-		printf( n);
-	
+		printf("%d", n);
+	printf("\n");
 	return (0);
 }
 
