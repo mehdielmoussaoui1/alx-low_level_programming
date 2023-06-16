fb568706c472957df7a5263a4caf765964f6e58f
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-16 06:35:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,16 @@
  * followed by a new line
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void)
+{
     char alphabet;
 
-    for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+    for(alphabet ='a'; alphabet <='z'; alphabet++) 
+    {
         putchar(alphabet);
     }
 
     putchar('\n');
 
-    return 0;
+    return (0);
 }
