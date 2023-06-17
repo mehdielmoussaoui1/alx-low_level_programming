3e66bcebdbf4d3c07621c8a440e2b020b6408641
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-17 13:32:42 -0700

    print_comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
 
 /**
-  * main - Prints 3 combination of numbers
+  * main -prints all possible combinations of two two-digit numbers.
   *
-  * Return: Always (Success)
+  * Return: 0 (Success)
   */
-int main() {
-    int i, j;
+int main(void)
+{
+	int a, b, c, d;
 
-    for (i = 0; i <= 99; i++) {
-        int f = i / 10;
-        int s = i % 10;
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (((c + d) > (a + b) &&  c >= a) || a < c)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(' ');
+						putchar(c);
+						putchar(d);
 
-        for (j = i; j <= 99; j++) {
-            int r = j / 10;
-            int t = j % 10;
+					if (a + b + c + d == 227 && a == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
 
-            putchar(f / 10 + '0');
-            putchar(f % 10 + '0');
-            putchar(s / 10 + '0');
-            putchar(s % 10 + '0');
-            putchar(' ');
-            putchar(r / 10 + '0');
-            putchar(r % 10 + '0');
-            putchar(t / 10 + '0');
-            putchar(t % 10 + '0');
+	putchar('\n');
 
-            if (j < 99) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    return 0;
+	return (0);
 }
