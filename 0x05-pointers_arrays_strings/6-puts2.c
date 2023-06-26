c7dff4009af286e2c54872c8f6342561d0fb7b59
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-26 14:01:47 -0700

    putts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,23 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int p = 0;
+	int t = 0;
+	char *x = str;
+	int o;
 
-	while (*(str + i) != '\0')
+	while (*x != '\0')
 	{
-		if (i % 2 == 0)
-			putchar(*(str + i));
-		i++;
+		x++;
+		p++;
 	}
-	putchar(10);
+	t = p - 1;
+	for (n = 0 ; n <= t ; n++)
+	{
+		if (n % 2 == 0)
+	{
+		_putchar(str[n]);
+	}
+	}
+	_putchar('\n');
 }
