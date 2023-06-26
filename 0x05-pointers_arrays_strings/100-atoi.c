22ecff14692eddc6bf81c4f0ab0366b2c06e8dba
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-26 15:28:33 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,20 +6,19 @@
 *
 * Return: the converted int
 */
-
 int _atoi(char *s)
 {
 	int sign = 1;
-	unsigned int n = 0;
+	unsigned int num = 0;
 
 	do {
 		if (*s == '-')
-			c *= -1;
+			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			n = n * 10 + (*s - '0');
-		else if (n > 0)
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
 			break;
 	} while (*s++);
 
-	return (n * c);
+	return (num * sign);
 }
