faef2bd868b6bf12458d026fe9c03ac677da0d7e
mehdielmoussaoui1 <elmoussaoui.mehdi7@gmail.com>
2023-06-27 02:04:18 -0700

    kygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: generated password
+ */
+
+int main(void)
+{
+	char c;
+	int x;
+
+	srand(time(0));}
